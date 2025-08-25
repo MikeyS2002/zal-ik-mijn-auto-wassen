cf162ab7a76a0649504dadd80e60d649a9225a47
mikeXmontagne <mike@montagneagency.com>
2025-08-25 18:48:24 +0200

    rediss test

--- app/api/daily-update/route.js
+++ app/api/daily-update/route.js
@@ -4,8 +4,8 @@ import { Redis } from "@upstash/redis";
 
 // Initialize Redis connection
 const redis = new Redis({
-    url: process.env.REDIS_URL,
-    token: process.env.KV_REST_API_TOKEN,
+    url: process.env.UPSTASH_REDIS_REST_URL,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
 
 export async function GET() {
